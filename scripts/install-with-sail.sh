0e80002750de7a075f0aa13e413ed740722549cb
Kevin Alcantara <kevinalcantara37@gmail.com>
2022-04-14 20:06:45 -0300

    Fix docker php version and add install scripts

---
+++ scripts/install-with-sail.sh
@@ -0,0 +1,12 @@
+echo "Installing Project..."
+
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v $(pwd):/var/www/html \
+    -w /var/www/html \
+    laravelsail/php74-composer:latest \
+    composer install --ignore-platform-reqs
+cp .env.example .env
+./vendor/bin/sail up -d && ./vendor/bin/sail artisan key:generate
+
+echo "Project Installed!"
